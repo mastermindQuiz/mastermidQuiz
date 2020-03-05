8a2adafbb8b1a77a3c290bad91aa0d13418b6ea3
maria-roman-garcia <mromangarcia04@gmail.com>
2020-03-05 21:30:39 +0100

    podemos cargar las preguintas y sumar y restar puntos

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
 // Objeto plantilla
-let selectedObject = []
-
+let selectedObject = [];
+let score = 0;
 
 fetch('preguntas.json')
     .then(response => response.json())
     .then((responseJson) => {
 
-        let jsonObjects = responseJson
+        let jsonObjects = responseJson;
         let numberOfQuestions = jsonObjects.length - 1;
 
+        // Empezamos
+        start = () => {
+            selectedObject = [];
+            document.getElementById("question").innerHTML = "";
+            document.getElementById("answer").innerHTML = "";
+            getRandomNumber();
+        }
+
+        let randomNumber;
         // funcion que selecciona un numero al azar desde 0 hasta el numero de preguntas que tengamos
         getRandomNumber = () => {
-            let randomNumber = Math.floor(Math.random() * numberOfQuestions)
-            pushObject(randomNumber)
+            randomNumber = Math.floor(Math.random() * numberOfQuestions);
+            pushObject(randomNumber);
         }
         // Metemos en selectedObject el objeto de indice de randomNumber.
-        pushObject = (patata) => {
-            selectedObject.push(jsonObjects[patata])
+        pushObject = (data) => {
+            selectedObject.push(jsonObjects[data]);
+            jsonObjects.splice(data, 1);
+            selectedObject.respuestas = [selectedObject[0].correcta, ...selectedObject[0].incorrecta];
+            showInfo();
         }
-        
-
+        // Pintamos la informacion en el html
+        showInfo = () => {
+            document.getElementById("question").innerHTML += "<p>" + selectedObject[0].pregunta + "</p>"
+            selectedObject.respuestas.forEach((element, index) => {
+                document.getElementById("answer").innerHTML += "<button id=" + index + " onclick='compareAnswer(this)'>" + element + "</button>"
+            });
+        }
+        //Comparamos el contenido del boton seleccionado con la respuesta correcta
+        compareAnswer = (e) => {
+            let respuesta = e.innerHTML
+            if (respuesta === selectedObject[0].correcta) {
+                score += 10;
+                document.getElementById("points").innerHTML = "<p>"+score +" pts"+"</p>"
+                console.log(score)
+                console.log("modal CORRECTO!");
+            } else {
+                if (score > 0) {
+                    score += -2;
+                     document.getElementById("points").innerHTML = "<p>" + score + " pts" + "</p>"
+                } else {}
+                console.log(score)
+                e.style.display = "none";
+            }
+        }
 
     })
     .catch(error => console.log(error))
-
-
-// $.getJSON("./preguntas.json", function (json) {
-//     console.log(json); // this will show the info it in firebug console
-// });
