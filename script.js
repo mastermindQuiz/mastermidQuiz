a876d3f91266b92931207f1d7887dcc0d4e7512a
jciria74 <47560520+jciria74@users.noreply.github.com>
2020-03-05 17:56:39 +0100

    iniciando js

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 
-fetch ('preguntas.json')
+let question = [];
+let answer = [];
+let correctAnswer =[];
+let numberOfQuestions
 
+fetch ('preguntas.json')
 .then (response => response.json())
-.then(responseJson => console.log(responseJson))
+.then((responseJson) => {
+    
+   
+        responseJson.map((elemento)=>{
+            question.push(elemento.pregunta);
+            answer.push(elemento.incorrecta);
+            correctAnswer.push(elemento.correcta);
+            
+        })
+        
+
+            
+            numberOfQuestions = question.length - 1;
+
+            let randomNumber = Math.floor(Math.random()* numberOfQuestions)
+            console.log(randomNumber)
+
+ 
+
+
+
+
+
+
+
+
+        
+    
+    })
 .catch(error => console.log(error))
 
 // $.getJSON("./preguntas.json", function (json) {
 //     console.log(json); // this will show the info it in firebug console
 // });
+
+
+
 
